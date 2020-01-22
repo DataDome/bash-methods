e3ea18c8d350033f75368f3afd1135c17d693417
Louis Paret <louis.paret@gmail.com>
2020-01-22 23:28:05 +0100

    Add blank line in printf commands (#4)

--- env-file/source-dot-env-file.sh
+++ env-file/source-dot-env-file.sh
@@ -3,7 +3,7 @@
 # Check if the file .env exists, if not it stops the script
 # The check is bypassed if we are on Jenkins
 if [[ ! -f .env && ! ${JENKINS_HOME} ]]; then
-    printf 'Please create the file .env from the template .env_template and by adding your credentials.'
+    printf 'Please create the file .env from the template .env_template and by adding your credentials.\n'
     exit 1
 fi
 
--- prepare.sh
+++ prepare.sh
@@ -9,13 +9,13 @@ cd "${BASEDIR}" || exit
 export BASH_METHODS_PATH="${BASEDIR}/bash-methods"
 
 if [ -d "${BASH_METHODS_PATH}" ]; then
-    printf 'Directory %s exists. Update source of the repository.' "${BASH_METHODS_PATH}"
+    printf 'Directory %s exists. Update source of the repository.\n' "${BASH_METHODS_PATH}"
     # Update source of the repository
     cd "${BASH_METHODS_PATH}"
     git pull
     cd "${BASEDIR}"
 else
-    printf 'Clone source of the repository.'
+    printf 'Clone source of the repository.\n'
     # Clone bash-methods repo
     git clone https://github.com/DataDome/bash-methods.git "${BASH_METHODS_PATH}"
 fi
