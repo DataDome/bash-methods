2dfd875481b57ff0a7ff60b65571010c3af10535
Pierre Carles <pierre.carles@datadome.co>
2020-02-11 11:26:04 +0100

    Add fd redirection in terraform scripts

--- terraform/get-script-parameters.sh
+++ terraform/get-script-parameters.sh
@@ -12,3 +12,10 @@ export STEP=$1; shift
 # Get all other command options
 # We use it to add other arguments to terraform commands
 export COMMAND_OPTIONS="$@"
+
+# When we run a `terraform output` command, we want the script to only display
+# the output of terraform, not the logs of the script.
+# For this we use fd redirection
+if [ "$STEP" = "output" ]; then
+    exec 3>&1 1>/dev/null # Save `value` of stdout in file descriptor 3 and redirect stdout to /dev/null
+fi
--- terraform/run-docker-compose-with-exit-code.sh
+++ terraform/run-docker-compose-with-exit-code.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-printf 'Launch Terraform script.\n'
-docker-compose pull
-docker-compose up \
-    --build \
-    --abort-on-container-exit \
-    --exit-code-from terraform \
-    terraform
+docker-compose pull --quiet
+printf '\n========================\n# Run Terraform script #\n========================\n\n'
+if true 2>/dev/null >&3; then # If the file descriptor 3 is open, we
+    exec 1>&3 3>&-            # restore value of stdout and close fd 3
+fi
+
+docker-compose run --rm terraform
