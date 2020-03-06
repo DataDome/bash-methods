6ac1ab8a132cb5c93fdb3f2b0f26467cb2408037
Pierre Carles <pierre.carles@datadome.co>
2020-03-06 17:49:37 +0100

    Change output of all error message to stderr

--- env-file/source-dot-env-file.sh
+++ env-file/source-dot-env-file.sh
@@ -3,7 +3,7 @@
 # Check if the file .env exists, if not it stops the script
 # The check is bypassed if we are on Jenkins
 if [[ ! -f .env && ! ${JENKINS_HOME} ]]; then
-    printf 'Please create the file .env from the template .env_template and by adding your credentials.\n'
+    printf 'Please create the file .env from the template .env_template and by adding your credentials.\n' >&2
     exit 1
 fi
 
